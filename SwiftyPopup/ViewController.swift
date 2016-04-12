5b7d001b0db21190f05a8f7d292e825e4b31e985
ehowlsla <jungler@cvdv.me>
2016-04-12 14:49:25 +0900

    Initial Commit

---
+++ SwiftyPopup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyPopup
+//
+//  Created by YoonTaesup on 2016. 4. 12..
+//  Copyright © 2016년 YoonTaesup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
